432105fd42c101e3f4d3ad1a9f226f4ae955100b
Youngchae James Chee <litcoderr@gmail.com>
2020-06-21 16:10:01 +0900

    finished script for upload and run

---
+++ run.bash
@@ -0,0 +1,32 @@
+ROOT_DIR=""
+BRANCH=""
+SCRIPT=""
+
+# Extract Argument
+while getopts r:b:c: flag
+do
+	case "${flag}" in
+		r) ROOT_DIR=${OPTARG};;
+		b) BRANCH=${OPTARG};;
+		s) SCRIPT=${OPTARG};;
+	esac
+done 
+
+if [ "$ROOT_DIR" == "" ] || [ "$BRANCH" == "" ] || [ "$SCRIPT" == "" ]; then
+	echo Set -r -b -s
+else
+	echo --------------------------------
+	echo Root Directory: $ROOT_DIR
+	echo Branch: $BRANCH
+	echo Script: $SCRIPT
+	echo running...
+	echo --------------------------------
+
+	TOOL_DIR="${PWD}";
+	cd $ROOT_DIR
+	# git pull from remote github server
+	git pull origin $BRANCH
+	# run script
+	$SCRIPT
+	cd $TOOL_DIR
+fi
---
+++ upload.bash
@@ -0,0 +1,37 @@
+
+ROOT_DIR=""
+BRANCH=""
+COMMENT="dev: upload to test server"
+
+# Extract Argument
+while getopts r:b:c: flag
+do
+	case "${flag}" in
+		r) ROOT_DIR=${OPTARG};;
+		b) BRANCH=${OPTARG};;
+		c) COMMENT=${OPTARG};;
+	esac
+done 
+
+if [ "$ROOT_DIR" == "" ] || [ "$BRANCH" == "" ]; then
+	echo Set -r -b 
+else
+	echo --------------------------------
+	echo Root Directory: $ROOT_DIR
+	echo Branch: $BRANCH
+	echo Comment: $COMMENT
+	echo uploading...
+	echo --------------------------------
+
+	TOOL_DIR="${PWD}";
+	cd $ROOT_DIR
+	# git commit current changes
+	git add .
+	git status
+	git commit -m "$COMMENT"
+	# git pull first
+	git pull origin $BRANCH
+	# git push
+	git push origin $BRANCH
+	cd $TOOL_DIR
+fi
